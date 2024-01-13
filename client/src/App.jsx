1fd5e62d6c46b0390a5310078366227c349dbfdb
gnanachandra <vvit.20bq1a05p2@gmail.com>
2024-01-13 13:09:26 +0530

    client setup complete

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react";
+
+const App = () => {
+  return (
+    <div className="container flex items-center justify-center w-full h-screen text-2xl font-semibold text-white bg-red-500">
+      <Button>Hello world</Button>
+    </div>
+  );
+};
+
+export default App;
