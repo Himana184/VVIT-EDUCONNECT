39be8d74277c06f43fb127ebe130fb64afe2bf46
gnanachandra <vvit.20bq1a05p2@gmail.com>
2024-01-19 20:01:10 +0530

    server folder setup & custom error middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 import express from "express";
+import { ErrorMiddleWare } from "./middleware/error.js";
+import connectDB from "./db/connect.js";
+import dotenv from "dotenv"
+
+//configure the env variable from the root path of the server (filename: .env)
+dotenv.config();
 
 const app = express();
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-});
+
+//custom error middleware
+app.use(ErrorMiddleWare);
+
+const PORT = process.env.PORT || 5000
+
+//connect to DB and start server
+const start = async () => {
+  try {
+    await connectDB(process.env.DATABASE_URI);
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
