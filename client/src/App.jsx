753bf95f74c065e57ec56f9e7dd00c82230e749d
gnanachandra <vvit.20bq1a05p2@gmail.com>
2024-02-04 14:27:14 +0530

    shadcn ui setup complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { Button } from "@material-tailwind/react";
+import { Button } from "./components/ui/button"
+
 
 const App = () => {
   return (
-    <div className="container flex items-center justify-center w-full h-screen text-2xl font-semibold text-white bg-red-500">
-      <Button>Hello world</Button>
+    <div>
+      <Button>click me</Button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "@material-tailwind/react";
-import store from "./store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
