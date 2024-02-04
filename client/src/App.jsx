38a47620d5fc01e4515648a97e66863af327c6bf
gnanachandra <vvit.20bq1a05p2@gmail.com>
2024-02-04 15:10:27 +0530

    react router dom and redux toolkit setup complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,34 @@
-import { Button } from "./components/ui/button"
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Landing from "./pages/Landing"
 const App = () => {
   return (
-    <div>
-      <Button>click me</Button>
-    </div>
+    <Router>
+      <Routes>
+
+        {/* Landing page which contains details about the project features */}
+        <Route path="/" element={<Landing />} />
+
+        {/* routes related to registeration and login of users */}
+        <Route path="/auth">
+          <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+        </Route>
+
+        {/* all routes of student */}
+        <Route path="/student"></Route>
+
+        {/* all routes of admin */}
+        <Route path="/admin"></Route>
+
+        {/* all routes of coordinator */}
+        <Route path="/coordinator"></Route>
+
+        {/* all routes of faculty */}
+        <Route path="/faculty"></Route>
+      </Routes>
+    </Router>
   )
 }
 
