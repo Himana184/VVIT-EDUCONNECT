aa0f4eef439a87cf57b6b8c418fd3737038ffddf
gnanachandra <vvit.20bq1a05p2@gmail.com>
2024-02-21 13:50:34 +0530

    add internships table and card view

--- client/src/pages/admin/Internships.jsx
+++ client/src/pages/admin/Internships.jsx
@@ -1,24 +1,30 @@
 import InternshipCard from '@/components/internships/InternshipCard'
 import TanstackTable from '@/components/table/TanstackTable';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
-import { studentTableColumns, studentsData } from '@/data/students';
-import React, { useState } from 'react'
-
+import { internshipTableColumns, internshipsData } from '@/data/internships';
+import { useQueryParams } from '@/hooks/useQueryParams';
+import { BuildingIcon } from 'lucide-react';
+import { useState } from 'react'
+// import { useLocation, useNavigate } from 'react-router-dom';
 const Internships = () => {
-  const [tableView, setTableView] = useState(false);
-
+  // const params = useQueryParams();
+  const [view, setView] = useState("card")
   return (
-    <div className='flex flex-col space-y-6'>
-      <div className='flex items-center space-x-2 justify-end'>
-        <Switch onCheckedChange={e => setTableView(e)} />
-        <Label>Table View</Label>
+    <>
+      <div className='flex flex-col space-y-6'>
+        <div className='flex items-center space-x-2 justify-end'>
+          <Switch onCheckedChange={e => {
+            e ? setView("table") : setView("card")
+          }} defaultChecked={view == "table"} />
+          <Label>Table View</Label>
+        </div>
+        {
+          view == "table" ? <TanstackTable tableData={internshipsData} columns={internshipTableColumns} /> : <InternshipCard />
+        }
       </div>
-      {
-        tableView == true ? <TanstackTable tableData={studentsData} columns={studentTableColumns} /> : <InternshipCard />
-      }
-    </div>
-
+    </>
   )
 }
 
