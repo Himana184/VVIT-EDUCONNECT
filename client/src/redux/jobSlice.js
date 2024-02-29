da20f22513b8498f8e91a28a7b98814f4999b584
Himana184 <20bq1a05l1@vvit.net>
2024-02-29 16:36:13 +0530

    student jobdrive integration

--- client/src/redux/jobSlice.js
+++ client/src/redux/jobSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
 
 export const handleAddJobDrive = createAsyncThunk(
-  "/api/v1/job",
+  "/api/v1/jobdrive(post)",
   async (payload, { rejectWithValue }) => {
     try {
       const response = await axios.post("/api/v1/jobdrive", payload, {
@@ -11,6 +11,25 @@ export const handleAddJobDrive = createAsyncThunk(
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
+      return response.data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+export const getJobDrives = createAsyncThunk(
+  "/api/v1/jobdrive(get)",
+  async (payload, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("/api/v1/jobdrive", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      console.log("Get jobdrive response : ", response);
       return response.data;
     } catch (error) {
       if (!error?.response) {
@@ -43,6 +62,20 @@ const jobSlice = createSlice({
       state.isLoading = false;
       console.log(payload);
       toast.error(payload?.message || "Something went wrong");
+    });
+
+    //get jobdrives
+    builder.addCase(getJobDrives.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getJobDrives.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.jobs = payload.data.jobs;
+      toast.success(payload.message);
+    });
+    builder.addCase(getJobDrives.rejected, (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload.message || "something went wrong");
     });
   },
 });
